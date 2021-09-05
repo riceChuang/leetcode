01805e1300614b510d951260041d839767047867
jason_chuang <jason_chuang@gmail.com>
2021-09-05 22:01:09 +0800

    question 13

---
+++ category/string/easy/romantointerger13/code.go
@@ -0,0 +1,26 @@
+package romantointerger13
+
+func romanToInt(s string) int {
+	var normalValue = make(map[string]int, 7)
+	normalValue["I"] = 1
+	normalValue["V"] = 5
+	normalValue["X"] = 10
+	normalValue["L"] = 50
+	normalValue["C"] = 100
+	normalValue["D"] = 500
+	normalValue["M"] = 1000
+
+	var result int
+	var lastKeyValue int
+	for _, runeValue := range s {
+		if value, ok := normalValue[string(runeValue)]; ok {
+			result += value
+			if (value-lastKeyValue) > 0 && ((value-lastKeyValue)%4 == 0 || (value-lastKeyValue)%9 == 0) {
+				result -= lastKeyValue * 2
+			}
+			lastKeyValue = value
+		}
+	}
+
+	return result
+}
