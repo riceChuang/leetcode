9e34e8055d6e3b22fd6e72f231ce4776c4be8381
jason_chuang <jason_chuang@gmail.com>
2021-09-05 22:31:37 +0800

    question14

---
+++ category/string/easy/longestcommonprefix14/code.go
@@ -0,0 +1,30 @@
+package longestcommonprefix14
+
+import "strings"
+
+func longestCommonPrefix(strs []string) string {
+
+	var result string
+
+	for index, value := range strs {
+		if index == 0 {
+			result = value
+			continue
+		}
+		if result == "" {
+			break
+		}
+
+		for {
+			if !strings.HasPrefix(value, result) {
+				if len(result) > 0 {
+					result = result[:len(result)-1]
+				}
+			} else {
+				break
+			}
+		}
+
+	}
+	return result
+}
---
+++ category/string/easy/longestcommonprefix14/code_test.go
@@ -0,0 +1,29 @@
+package longestcommonprefix14
+
+import "testing"
+
+func Test_longestCommonPrefix(t *testing.T) {
+	type args struct {
+		strs []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			"test",
+			args{
+				strs: []string{"flower","flow","flight"},
+			},
+			"fl",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := longestCommonPrefix(tt.args.strs); got != tt.want {
+				t.Errorf("longestCommonPrefix() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
