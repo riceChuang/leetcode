dfe8839c9e56ac4c9c420d9e5a709f87d30561f6
jason_chuang <rice8266@gmail.com>
2022-03-29 11:53:41 +0800

    141.LinkedListCycle

---
+++ category/hashtable/easy/141.LinkedListCycle/code.go
@@ -0,0 +1,27 @@
+package leetcode
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func hasCycle(head *ListNode) bool {
+	fast := head
+	slow := head
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+		if fast == slow {
+			return true
+		}
+	}
+	return false
+}
