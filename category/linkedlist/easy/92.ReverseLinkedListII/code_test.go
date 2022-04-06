ff0b0303519f81de047e90cb41efd53e346dd2c3
jason_chuang <rice8266@gmail.com>
2022-04-06 17:22:01 +0800

    92.ReverseLinkedList

---
+++ category/linkedlist/easy/92.ReverseLinkedListII/code_test.go
@@ -0,0 +1,58 @@
+package leetcode
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_reverseBetween(t *testing.T) {
+	type args struct {
+		head  *ListNode
+		left  int
+		right int
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		{"test",
+			args{
+				head:  getLink(),
+				left:  2,
+				right: 5,
+			},
+			&ListNode{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := reverseBetween(tt.args.head, tt.args.left, tt.args.right); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("reverseBetween() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func getLink() *ListNode {
+
+	return &ListNode{
+		Val: 1,
+		Next: &ListNode{
+			Val: 2,
+			Next: &ListNode{
+				Val: 3,
+				Next: &ListNode{
+					Val: 4,
+					Next: &ListNode{
+						Val: 5,
+						Next: &ListNode{
+							Val: 6,
+						},
+					},
+				},
+			},
+		},
+	}
+
+}
