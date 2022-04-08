2a4c2d0b26715b1c32a4ce05573eb4c7d898e1ee
jason_chuang <rice8266@gmail.com>
2022-04-08 11:34:55 +0800

    160.IntersectionTwoLinkedLists

---
+++ category/linkedlist/easy/160.IntersectionofTwoLinkedLists/code.go
@@ -0,0 +1,40 @@
+package leetcode
+
+import "fmt"
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+
+	a := headA
+	b := headB
+
+	for a != b {
+
+		if a == nil {
+			a = headB
+		} else {
+			a = a.Next
+		}
+
+		if b == nil {
+			b = headA
+		} else {
+			b = b.Next
+		}
+
+		fmt.Printf("Now a is:%v, b is:%v \n", a, b)
+	}
+	return a
+}
